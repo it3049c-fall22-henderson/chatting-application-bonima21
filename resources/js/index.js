c7f9980876dbc5a3717e5689dbbfc44185c504cb
Miriam Boni <bonima@mail.uc.edu>
2022-10-01 01:29:03 -0400

    added dark mode to index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,64 +1,70 @@
-const nameInput = document.getElementById("myName");
-const myMessage = document.getElementById("myMessage");
-const sendButton = document.getElementById("sendButton");
+const myMessage = document.getElementById("my-message");
+const sendButton = document.getElementById("send-button");
 const chatBox = document.getElementById("chat");
-const request = new XMLHttpRequest();
-const preElement = document.getElementById(`result`);
+const nameInput = document.getElementById("my-name-input");
+const save = document.getElementById("save")
 
 async function updateMessages() {
   const messages = await fetchMessages();
   let formattedMessages = "";
-  messages.forEach((message) => {
+  messages.forEach(message => {
     formattedMessages += formatMessage(message, nameInput.value);
   });
   chatBox.innerHTML = formattedMessages;
-  console.log(messages);
 }
 
-const serverURL = "https://it3049c-chat-application.herokuapp.com/messages";
+const serverURL = `https://it3049c-chat-application.herokuapp.com/messages`;
+
 function fetchMessages() {
-  return fetch(serverURL).then((Response) => Response.json());
+  return fetch(serverURL)
+    .then(response => response.json())
 }
-function formatMessage(message, myName) {
+
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
+
+function formatMessage(message, nameInput) {
   const time = new Date(message.timestamp);
   const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
 
-  if (myName === message.sender) {
+  if (nameInput === message.sender) {
     return `
-      <div class="mine messages">
-          <div class="message">
-              ${message.text}
-          </div>
-          <div class="sender-info">
-              ${formattedTime}
-          </div>
-      </div>
-      `;
+    <div class="mine messages">
+        <div class="message">
+            ${message.text}
+        </div>
+        <div class="sender-info">
+            ${formattedTime}
+        </div>
+    </div>
+    `
   } else {
     return `
-          <div class="yours messages">
-              <div class="message">
-                  ${message.text}
-              </div>
-              <div class="sender-info">
-                  ${message.sender} ${formattedTime}
-              </div>
-          </div>
-      `;
+        <div class="yours messages">
+            <div class="message">
+                ${message.text}
+            </div>
+            <div class="sender-info">
+                ${message.sender} ${formattedTime}
+            </div>
+        </div>
+    `
   }
 }
+
 function sendMessages(username, text) {
   const newMessage = {
     sender: username,
     text: text,
-    timestamp: new Date(),
-  };
+    timestamp: new Date()
+  }
+
   fetch(serverURL, {
     method: `POST`,
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json'
     },
-    body: JSON.stringify(newMessage),
+    body: JSON.stringify(newMessage)
   });
 }
 
@@ -70,16 +76,20 @@ sendButton.addEventListener("click", function (sendButtonClickEvent) {
   sendMessages(sender, message);
   myMessage.value = "";
 });
-const saveNameButton = document.getElementById('save-name');
-const messageInput = document.getElementById('messageInput');
 
-saveNameButton.addEventListener("click", function() {
-    localStorage.setItem('name', nameInput.value);
-});
-setInterval(function() {
-  if (!localStorage.getItem('name')){
-      messageInput.classList.add('d-none');
-  } else {
-      messageInput.classList.remove('d-none');
-  }
-}, 100);
+
+save.onclick = function () {
+  localStorage.setItem("my-name-input", nameInput.value);
+  nameInput.value = "";
+  document.getElementById("my-message").disabled = false;
+}
+
+edit.onclick = function () {
+  const text = localStorage.getItem("my-name-input")
+  nameInput.value = text;
+}
+
+function switchModes() {
+  var element = document.body;
+  element.classList.toggle("dark");
+}
